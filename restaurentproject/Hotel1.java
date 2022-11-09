a9cf96cdd3f80be7f55ff1512dadc127e10a10b2
PratikNar <117707920+PratikNar@users.noreply.github.com>
2022-11-09 19:42:06 +0530

    File Uploaded 9/11/2022

---
+++ restaurentproject/Hotel1.java
@@ -0,0 +1,62 @@
+package com.tyss.restaurentproject;
+
+import java.util.Scanner;
+
+public class Hotel1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To The Bajaj Hotel");
+		System.out.println("**********Atithi Devo Bhava*********");
+		Scanner input = new Scanner(System.in);
+		String[] s = new String[]        {"Bottle  ","Cofee    ","Dosa    ","Burger     ","Wadapav   ","Quit     "};
+		int[] rate = new int[]{30,35,40,30,25,0};
+		int[] qt = new int[10];
+	        int sum=0;
+		boolean quit=true;
+	         
+	        
+	        do{
+	            System.out.println("ITEM"+"\t\t\tPrice");
+			for(int i=0;i<6;i++)
+	            System.out.println((i+1)+"."+s[i]+"\t\t"+rate[i]);
+	            
+	            System.out.println("Order Your Food");
+	            int ch=input.nextInt();
+		    if(ch>0 && ch<6)
+		    {
+			System.out.println("enter the no of quantites of "+s[ch-1]);
+	                 int q=input.nextInt();
+		         qt[ch-1]+=q;
+			
+		    }
+		    else
+		    {
+	               quit=false;
+	               throw new InvalidFoodException("Press the coreect No");
+	              
+	            }
+	        
+	        }while(quit);
+		
+		   
+	        System.out.println("Your Orders are:\n");
+		    for(int i=0;i<10;i++)
+		    {
+	             if(qt[i]!=0)
+		     {
+		      sum+=qt[i]*rate[i];
+	              System.out.println(s[i]+"*   "+qt[i]+"=="+qt[i]*rate[i]+"rs");
+		     }
+		    }
+	  
+	        
+	        System.out.println("Your total bill="+sum);
+	        
+	         System.out.println("Thank you");
+	    
+	     
+	    }
+	    
+	
+	}
+
---
+++ restaurentproject/InvalidFoodException.java
@@ -0,0 +1,11 @@
+package com.tyss.restaurentproject;
+
+public class InvalidFoodException extends RuntimeException {
+
+	public InvalidFoodException(String s) {
+		// TODO Auto-generated constructor stub
+		super(s);
+		
+	}
+
+}
